36dadfea8839b0604b6035a5920187af0632c707
lmfmfombeo <2004.moras@gmail.com>
2025-01-11 08:39:50 -0800

    website backend

--- Backend/Index.js
+++ Backend/Index.js
@@ -2,7 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import cors from 'cors';
-import userRoutes from '../routes/userRoutes.js';
+import userRoutes from './Routes/userRoutes.js';
 
 dotenv.config();
 
--- Backend/Routes/userRoutes.js
+++ Backend/Routes/userRoutes.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import bcrypt from 'bcrypt';
-import User from './models/userModel.js';
+import User from '../models/userModel.js';
+
 
 const router = express.Router();
 
